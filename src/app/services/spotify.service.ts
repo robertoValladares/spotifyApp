7969f6464b26829c4c581e1b9a7f20a2156484fa
Roberto Valladares <robertvalladares20@gmail.com>
2024-01-12 21:10:01 -0600

    artist component

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -48,8 +48,8 @@ export class SpotifyService {
 
   // getTopTracks( id: string ) {
 
-  // //   return this.getQuery(`artists/${ id }/top-tracks?country=us`)
-  // //               .pipe( map( data => data['tracks']));
+  //   return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+  //               .pipe( map( data => data['tracks']));
 
   // // }
 
