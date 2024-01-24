bd69a6949f48c0fc593b423b188bf26a755dfa9f
Roberto Valladares <robertvalladares20@gmail.com>
2024-01-23 22:05:07 -0600

    interceptor

--- src/app/core/interceptor/token.interceptor.ts
+++ src/app/core/interceptor/token.interceptor.ts
@@ -1,5 +1,20 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpHeaders } from '@angular/common/http';
 
-export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class tokenInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+
+    const token = 'BQDsnU8IhPdDKrUXuWq7FVMlrNmZrmMJiSyG6fRESINYgAJBBpnLiQ6GThLMuQsxqlLJVavErt4qQAgzvrA';
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    const modifiedRequest = request.clone({
+      headers: headers
+    });
+
+    return next.handle(modifiedRequest);
+  }
+}
